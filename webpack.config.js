f1f5906a40789d56f506176e019b7313e3c2a9e9
runo4 <zukky1218>
2021-08-20 17:25:55 +0900

    2021/8/20 17:25 Procfile

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
     // モードの設定
-    mode: 'development',
+    mode: 'development', //development, production
     // エントリーポイントの設定
     entry: `./src/index.js`,
     // ファイルの出力設定
